b01f9106a481aca919eac6235e69ce284870b3c7
Gibus Wearing Mann <7275663+thecnoNSMB@users.noreply.github.com>
2022-04-04 09:51:24 -0400

    initial commit

---
+++ mod.js
@@ -0,0 +1,32 @@
+module.exports = {
+    title: "High Quality Rips Mod",
+    summary: "Replaces some music files with Siivagunner versions.",
+    author: "Gibus Wearing Brony",
+    modVersion: 0.2,
+
+    routes: {
+        //88 would go here //Harlequin
+        //90 would go here (the Namco High dadbattle rip is ideal) //Showtime
+        'assets://storyfiles/hs2/00246/00246.mp3': './music/00246.mp3', //Sburban Countdown
+        'assets://storyfiles/hs2/10246/10246.mp3': './music/00246.mp3',
+        'assets://storyfiles/hs2/00253/mspa_harlequin.mp3': './music/mspa_harlequin.mp3', //also affects 338
+        'assets://storyfiles/hs2/00253/afterthought-showtime_piano.mp3': './music/afterthought-showtime_piano.mp3',
+        //418 would go here //Nannaquin
+        'assets://storyfiles/hs2/00757/00757.mp3': './music/00757.mp3', //Explore
+        //843 would go here //Harleboss
+        //879 would go here //Harleboss
+        //918 would go here //Dissension
+        'assets://storyfiles/hs2/00980/00980_2.mp3': './music/00980_2.mp3', //Carefree Victory
+        'assets://storyfiles/hs2/01026/01026.mp3': './music/01026.mp3', //The Beginning of Something Really Excellent
+        //1358 would go here //Doctor
+        //3000 would go here //Sunslammer
+        'assets://storyfiles/hs2/03085/03085.mp3': './music/03085.mp3', //Savior of the Waking World
+        'assets://storyfiles/hs2/03294/03294.mp3': './music/03294.mp3', //MeGaLoVania
+        'assets://storyfiles/hs2/13294/13294.mp3': './music/03294.mp3',
+        //3757 would go here //BL1ND JUST1C3 : 1NV3ST1G4T1ON !!
+        'assets://storyfiles/hs2/04941/04941.mp3': './music/04941.mp3', //I'm A Member of the Midnight Crew (Acapella)
+        'assets://storyfiles/hs2/songs/alterniaboundsongs/MEGALOVANIA.mp3': './music/alterniaboundsongs/MEGALOVANIA.mp3',
+        'assets://storyfiles/hs2/songs/alterniaboundsongs/Horschestra.mp3': './music/alterniaboundsongs/Horschestra.mp3',
+        //other rips i'm not advanced enough for: killed by br8k spider, unite synchronization, horschestra strong version, trickster mode, oppa toby style, fuschia ruler
+    }
+}
